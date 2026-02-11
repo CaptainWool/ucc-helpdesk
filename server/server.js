efd62718c9a8a90eb9898043ac1ce264b2f74649
UCC Helpdesk Dev <ucc-helpdesk@example.com>
2026-02-11 09:50:27 +0000

    Switch SMTP to port 587 (STARTTLS) to resolve timeout

--- server/server.js
+++ server/server.js
@@ -25,8 +25,8 @@ const pool = new Pool({
 
 const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
-    port: 465,
-    secure: true, // Use SSL
+    port: 587,
+    secure: false, // Use STARTTLS
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS.replace(/\s+/g, '')
