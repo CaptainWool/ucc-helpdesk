3a0afdfb67dbaf2f8b4c312c173b26e7ee26b36c
UCC Helpdesk Dev <ucc-helpdesk@example.com>
2026-02-11 09:53:21 +0000

    Attempt #3: Use simplified gmail service

--- server/server.js
+++ server/server.js
@@ -24,12 +24,14 @@ const pool = new Pool({
 });
 
 const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 587,
-    secure: false, // Use STARTTLS
+    service: 'gmail',
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS.replace(/\s+/g, '')
+    },
+    tls: {
+        // Force IPv4 if IPv6 is failing
+        rejectUnauthorized: false
     }
 });
 
